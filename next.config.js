a4f4c7494437f8be22986348354327422a0dbcac
Novix <evandiamantopoulos@gmail.com>
2025-08-08 14:01:55 -0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, experimental: { appDir: true } };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {}
+};
+
 module.exports = nextConfig;
